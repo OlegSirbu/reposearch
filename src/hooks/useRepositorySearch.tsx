f211d3952300c4f1473c288400927a646032fe16
olegsyrbu <olegsirbu2@gmail.com>
2023-11-28 20:14:57 +0100

    fix problem with runtime error

--- src/hooks/useRepositorySearch.tsx
+++ src/hooks/useRepositorySearch.tsx
@@ -1,7 +1,7 @@
-import { useState, useCallback } from 'react';
-import { useQuery, ApolloError } from '@apollo/client';
-import { GET_REPOSITORIES } from 'src/graphql/queries';
-import { PAGE_SIZE } from 'src/constants';
+import { useState, useCallback } from "react";
+import { useQuery, ApolloError } from "@apollo/client";
+import { GET_REPOSITORIES } from "src/graphql/queries";
+import { PAGE_SIZE } from "src/constants";
 
 interface PageInfo {
   endCursor: string | null;
@@ -25,45 +25,50 @@ interface UseRepositorySearchProps {
 }
 
 export const useRepositorySearch = (): UseRepositorySearchProps => {
-  const [query, setQuery] = useState<string>('');
+  const [query, setQuery] = useState<string>("");
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [endCursor, setEndCursor] = useState<string | null>(null);
 
-  const { data, loading, error, fetchMore } = useQuery<QueryResult>(GET_REPOSITORIES, {
-    variables: { query, first: PAGE_SIZE, after: endCursor },
-  });
+  const { data, loading, error, fetchMore } = useQuery<QueryResult>(
+    GET_REPOSITORIES,
+    {
+      variables: { query, first: PAGE_SIZE, after: endCursor },
+    }
+  );
 
-  const updateQuery = useCallback((newQuery: string, newEndCursor: string | null, newCurrentPage: number) => {
-    setQuery(newQuery);
-    setEndCursor(newEndCursor);
-    setCurrentPage(newCurrentPage);
-  }, []);
+  const updateQuery = useCallback(
+    (newQuery: string, newEndCursor: string | null, newCurrentPage: number) => {
+      setQuery(newQuery);
+      setEndCursor(newEndCursor);
+      setCurrentPage(newCurrentPage);
+    },
+    []
+  );
 
-  const handleSearch = useCallback((newQuery: string) => {
-    updateQuery(newQuery, null, 1);
+  const handleSearch = useCallback(
+    (newQuery: string) => {
+      updateQuery(newQuery, null, 1);
+    },
+    [updateQuery]
+  );
 
-    fetchMore({
-      variables: {
-        query: newQuery,
-        first: PAGE_SIZE,
-        after: null,
-      },
-    });
-  }, [updateQuery, fetchMore]);
+  const handlePageChange = useCallback(
+    (page: number) => {
+      const newEndCursor =
+        (page !== 1 && data?.search?.pageInfo.endCursor) || null;
+      updateQuery(query, newEndCursor, page);
 
-  const handlePageChange = useCallback((page: number) => {
-    const newEndCursor = (page !== 1 && data?.search?.pageInfo.endCursor) || null;
-    updateQuery(query, newEndCursor, page);
-
-      newEndCursor && fetchMore({
-        variables: {
-          query,
-          first: PAGE_SIZE,
-          after: newEndCursor,
-        },
-      });
-  }, [query, data, updateQuery, fetchMore]);
-
+      newEndCursor &&
+        fetchMore({
+          variables: {
+            query,
+            first: PAGE_SIZE,
+            after: newEndCursor,
+          },
+        });
+    },
+    [query, data, updateQuery, fetchMore]
+  );
 
   return {
     data,
